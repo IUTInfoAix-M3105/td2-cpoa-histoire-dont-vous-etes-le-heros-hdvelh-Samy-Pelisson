c93888e59ee24b4a4ab35e5e5cdb62156f84ffd2
Samy-Pelisson <samypelissonyt@gmail.com>
2020-11-20 12:12:31 +0100

    Update Event.java

--- src/pracHDVELH/Event.java
+++ src/pracHDVELH/Event.java
@@ -17,10 +17,25 @@ public class Event extends NodeMultiple {
 	public static final String ERROR_MSG_UNEXPECTED_END = "Sorry, for some unexpected reason the story ends here...";
 	public static final String PROMPT_ANSWER = "Answer: ";
 	public static final String WARNING_MSG_INTEGER_EXPECTED = "Please input a integer within range!";
+	private String playerAnswer;
+	private GUIManager gui;
+	private Scanner reader;
+	private int chosenPath;
+	private int id;
+	static private int lastId = -1;
 
+
+	/*Constructeur par défaut*/
+	public Event() {
+		this(new GUIManager(), null);
+	}
 	/*Constructeur surchargé*/
-	public Event(String data) {
+	public Event(GUIManager gui, String data) {
 		super(data);
+		chosenPath = -1;
+		this.gui = gui;
+		this.reader = gui.getInputReader();
+		id = ++lastId;
 	}
 
 
@@ -123,6 +138,19 @@ public class Event extends NodeMultiple {
 	/* Methods */
 	/* TO BE COMPLETED */
 	}
+
+	public int interpretAnswer(String answer) {
+		
+	}
+
+	public Event run() {
+		gui.outputln(getData());
+		gui.output(PROMPT_ANSWER);
+		playerAnswer = reader.next();
+		chosenPath = interpretAnswer(playerAnswer);
+		return getDaughter(chosenPath);
+	}
+
 }
 
 // eof
